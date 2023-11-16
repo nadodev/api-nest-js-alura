bce1a170234dcee3cfc29cce8ad9c4673d9bb99e
Leonardo Geja <bemmenos@gmail.com>
2023-11-16 17:06:38 -0300

    Delete dist directory

--- dist/Usuarios/usuario.repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsuarioEntity } from './usuario.entity';
-export declare class usuarioRepository {
-    private users;
-    save(user: UsuarioEntity): Promise<void>;
-    findAll(): Promise<UsuarioEntity[]>;
-    emailUnique(email: string): Promise<boolean>;
-    updateUser(id: string, user: Partial<UsuarioEntity>): Promise<false | UsuarioEntity>;
-}
